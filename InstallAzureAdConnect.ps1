716b3ce9c08cd7c316b1771cefa4ced3ddcb80d0
MacMike <92017058+CobraKarl@users.noreply.github.com>
2022-03-31 10:13:26 +0200

    Update InstallAzureAdConnect.ps1

--- InstallAzureAdConnect.ps1
+++ InstallAzureAdConnect.ps1
@@ -1,4 +1,4 @@
 # DOWNLOAD AND INSTALL INSTALLERA AZURE AD CONNECT
 $DownloadPath = "$HOME/Downloads"
 Start-BitsTransfer https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi -Destination $DownloadPath
-Start-Process "AzureADConnect.msi"
+Start-Process $DownloadPath\AzureADConnect.msi
